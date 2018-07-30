2d3bfcee83506757177037a619ef3734f4092d28
Khawaja Farooq <khawaja.farooq@gib.com>
2018-07-30 22:56:48 +0300

    roadmap

--- TechRhyme.playground/Contents.swift
+++ TechRhyme.playground/Contents.swift
@@ -1,2 +1,16 @@
 
 //:# Data Structures & Algorithms
+//: This playground contains all the possible Data Structures and Algorithms
+//:## Data Structures
+//:1. Array
+//:2. LinkedList
+//:3. Stack
+//:4. Queue
+//:5. Tree
+//:6. Graph
+//:## Algorithms
+//:1. Searching
+//:2. Sorting
+//:3. Recursive
+//:4. Divide and Conquer
+//:5. Dynamic Programming
