889e4f0b3f85af5ece5487690c65abdbb641c4d4
Khawaja Farooq <kfarooqa@gmail.com>
2018-08-04 10:44:51 +0300

    Linkedlist: little more refactoring

--- TechRhyme.playground/Pages/LinkedList.xcplaygroundpage/Sources/LinkedList.swift
+++ TechRhyme.playground/Pages/LinkedList.xcplaygroundpage/Sources/LinkedList.swift
@@ -1,32 +1,5 @@
 import Foundation
 
-public class Node<T: Comparable> {
-    public let data: T
-    public var next: Node? = nil
-    
-    public init(data: T) {
-        self.data = data
-    }
-}
-
-extension Node: Equatable {
-    static public func ==(lhs: Node, rhs: Node) -> Bool {
-        return lhs.data == rhs.data && lhs.next == rhs.next
-    }
-}
-
-extension Node: Hashable {
-    public var hashValue: Int {
-        return Unmanaged.passUnretained(self).toOpaque().hashValue
-    }
-}
-
-extension Node: CustomStringConvertible {
-    public var description: String {
-        return "🔘: \(data)"
-    }
-}
-
 public class LinkedList<T: Comparable> {
     
     public var head: Node<T>?
