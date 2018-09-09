635f5e9411d4886e9f2cacc5649a1b28d7e39bce
Khawaja Farooq <kfarooqa@gmail.com>
2018-09-10 00:12:25 +0300

    implemented two stack problem

--- TechRhyme.playground/Pages/Stack.xcplaygroundpage/Contents.swift
+++ TechRhyme.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -2,27 +2,40 @@
 
 import Foundation
 
-var stack = Stack<Int>()
-stack.push(1)
-stack.push(2)
-stack.push(4)
-stack.push(3)
-stack.pop()
-//print(stack)
+//var stack = Stack<Int>()
+//stack.push(1)
+//stack.push(2)
+//stack.push(4)
+//stack.push(3)
+//stack.pop()
+////print(stack)
+//
+//
+//var myStack = MyStack<Int>()
+//myStack.push(1)
+//myStack.push(2)
+//myStack.push(4)
+//myStack.push(3)
+//myStack.pop()
+//
+//for item in myStack {
+//    print(item)
+//}
+//myStack.count
+//myStack.contains(4)
+//
 
-
-var myStack = MyStack<Int>()
-myStack.push(1)
-myStack.push(2)
-myStack.push(4)
-myStack.push(3)
-myStack.pop()
-
-for item in myStack {
-    print(item)
-}
-myStack.count
-myStack.contains(4)
+var ts = TwoStack<Int>()
+ts.push1(5)
+ts.push2(10)
+ts.push2(15)
+ts.push1(11)
+ts.push2(7)
+ts.pop1()
+ts.pop2()
+ts.pop2()
+ts.pop1()
+ts.pop2()
 //: [Next](@next)
 
 //: [Next](@next)
--- TechRhyme.playground/Pages/Stack.xcplaygroundpage/Sources/Stack.swift
+++ TechRhyme.playground/Pages/Stack.xcplaygroundpage/Sources/Stack.swift
@@ -93,4 +93,91 @@ extension MyStack: Collection {
     }
 }
 
+/*
+ 
+ top (index)
+ count
+ store sequentially
+ 
+ s1 = 2(top)
+ s2 = 5(top)
+ 2,5,3
+ swap top2 with top1 (index + array value)
+ 2,3,5
+ 
+ 2,3,5,8
+ 
+ append after top1, update top1, count1
+ append after top2, update top2, count1
+ 
+ pop - from top to count
+ 
+ 
+ stack = TwoStack
+ stack.push1(2)
+ stack.push2(5)
+ stack.push1(3)
+ stack.push2(8)
+ stack.push1(9)
+ stack.push1(5) - top1
+ stack.push2(7)
+ stack.push2(1) - top2
+ 
+ */
+
+public struct TwoStack<Element> {
+    private var elements: [Element] = []
+    
+    private var top1: Int = -1
+    private var top2: Int = -1
+    
+    private var count1: Int = 0
+    private var count2: Int = 0
+    
+    public init() {}
+    
+    public mutating func push1(_ value: Element) {
+        
+        top1 += 1
+        top2 += 1
+        count1 += 1
+        push(value, at: top1)
+    }
+    
+    public mutating func push2(_ value: Element) {
+        top2 += 1
+        count2 += 1
+        push(value, at: top2)
+    }
+    
+    public mutating func pop1() -> Element? {
+        top1 -= 1
+        top2 -= 1
+        count1 -= 1
+        return pop(at :top1+1)
+    }
+    
+    public mutating func pop2() -> Element? {
+        top2 -= 1
+        count2 -= 1
+        return pop(at :top2+1)
+    }
+    
+    
+    // MARK: - helper methods
+    private mutating func push(_ value: Element, at index: Int) {
+        elements.insert(value, at: index)
+    }
+    
+    private mutating func pop(at index: Int) -> Element? {
+        
+        guard index > -1 else { return nil }
+        return elements.remove(at: index)
+    }
+    
+}
+
+
+
+
 
