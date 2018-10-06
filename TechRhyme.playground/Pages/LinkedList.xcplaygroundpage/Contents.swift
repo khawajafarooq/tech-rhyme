538bad303c405ecc19e6aec0a4f99b30eb2485e1
Khawaja Farooq <khawaja.farooq@gib.com>
2018-10-06 17:57:29 +0300

    LinkedList add two numbers

--- TechRhyme.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
+++ TechRhyme.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -22,3 +22,44 @@ print("\(node!)")
 ll.reverse()
 print(ll)
 
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    
+    var list1 = l1
+    var list2 = l2
+    let dummy: ListNode? = nil
+    var result = dummy
+    var carry = 0
+    
+    while (list1 != nil || list2 != nil) {
+        
+        let val1 = list1 != nil ? list1?.val : 0
+        let val2 = list2 != nil ? list2?.val : 0
+            
+        let sum = val1! + val2! + carry
+        carry = sum / 10
+        let nodeValue = ListNode(sum % 10)
+        
+        result?.next = nodeValue
+        result = result?.next
+        if list1 != nil { list1 = list1?.next }
+        if list2 != nil { list2 = list2?.next }
+    }
+    
+    if carry > 0 {
+        result?.next = ListNode(carry)
+    }
+    
+    return dummy?.next
+}
+
+
